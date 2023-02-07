d20a68ec8d0c0bd84528351cdc0f81fe3f0dc866
Salvador Torres Rodriguez <a00825038@itesm.mx>
2023-02-06 18:34:23 -0600

    Se agrego estilo a sign in y sign up

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -10,21 +10,32 @@ let router = express()  // Instancia de express
 // Tengo que importar el Schema de 'Users'
 let User = require("../models/User")
 
-router.get("/", function(req, res){
-    res.render("signup")
+// Ruta 'get' para ir a la home page
+router.get("/", async function(req, res){
+    res.render("index")
 })
+
+// Ruta 'get' para ir a la pagina de 'sign up'
+router.get("/sign_up", async function(req, res){
+    res.render("sign_up2");
+});
 
 // Para enviarle datos al server, es por medio de un POST
 // Si el cliente me esta enviando una peticion POST a esta ruta ('/sign_up'), lo primero que tengo
 // que hacer es recuperar la info. La info viene en el CUERPO de la peticion.
 
 // Agregamos nuevo usuario a la base de datos
-router.post('/sign_up', async function(req, res){ // async sirve para que el codigo se detenga, hasta que se agregue el usuario
+router.post("/sign_up", async function(req, res){ // async sirve para que el codigo se detenga, hasta que se agregue el usuario
     
     let user = new User(req.body)
     await user.save()
     res.redirect("/")
 })
+
+// Ruta 'get' para ir a la pagina de 'sign in'
+router.get("/sign_in", async function(req, res){
+    res.render("sign_in");
+});
 
 module.exports = router // Esto es para que todos los objetos 'app' se puedan importar despues
                         // en otro lugar, el cual seria 'app.js'
